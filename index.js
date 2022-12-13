074605f225f131c826b443ec436d6d755ad8d0b6
KristinaRud <kristina.rud5@gmail.com>
2022-12-13 23:11:57 +0200

    changed

--- index.js
+++ index.js
@@ -7,29 +7,35 @@ form.addEventListener("click", (e) => {
   const iconTarget = e.target.closest("i");
 
   if (iconTarget) {
-    const input = iconTarget.parentNode.querySelector("input");
     iconTarget.classList.toggle("fa-eye");
     iconTarget.classList.toggle("fa-eye-slash");
 
-    const type = iconTarget.classList.contains("fa-eye-slash") ? "text" : "password";
-    input.setAttribute("type", type);
+    const type = iconTarget.classList.contains("fa-eye-slash")
+      ? "text"
+      : "password";
+    iconTarget.previousElementSibling.setAttribute("type", type);
   }
 });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (e.defaultPrevented) {
-    const firstInputValue = form.querySelector("#first").value;
-    const secondInputValue = form.querySelector("#second").value;
-    if (firstInputValue !== secondInputValue || firstInputValue === "" || secondInputValue === "") {
-        spanMsg.textContent = "Потрібно ввести однакові значення";
-    } else {
-      alert("You are welcome");
-    }
+
+  const firstInputValue = form.querySelector("#first").value;
+  const secondInputValue = form.querySelector("#second").value;
+  if (
+    firstInputValue !== secondInputValue ||
+    firstInputValue === "" ||
+    secondInputValue === ""
+  ) {
+    spanMsg.textContent = "Потрібно ввести однакові значення";
+  } else {
+    alert("You are welcome");
   }
 });
 
-form.addEventListener("focus",(e) => {
+form.addEventListener(
+  "focus",
+  (e) => {
     const target = e.target.closest("input");
     if (target) {
       spanMsg.textContent = "";
